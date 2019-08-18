79cdcae3664cb2c4cfbdabad3dfe988d0af839cd
Barbara Johnson <johnson.barbara@gmail.com>
2019-08-18 04:22:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 let currentUser = "Barbara"
-const welcomeMessage = "Welcome to Flatbook, " + currentUser!
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
